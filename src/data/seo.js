8973513d1f508f8863df13c8b254975368420985
Cyrus748 <adityanegi748@gmail.com>
2024-04-29 16:47:46 +0530

    Update Repository

---
+++ src/data/seo.js
@@ -0,0 +1,38 @@
+const SEO = [
+	{
+		page: "home",
+		description:
+			"I am a fullstack web developer with expertise in Frontend. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies.",
+		keywords: ["Aditya", "Aditya N.", "Aditya Negi"],
+	},
+
+	{
+		page: "about",
+		description:
+			"I am a fullstack web developer with expertise in Frontend. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies.",
+		keywords: ["Aditya", "Aditya N.", "Aditya Negi"],
+	},
+
+	{
+		page: "articles",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+		keywords: ["Aditya", "Aditya N.", "Aditya Negi"],
+	},
+
+	{
+		page: "projects",
+		description:
+			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to.",
+		keywords: ["Aditya", "Aditya N.", "Aditya Negi"],
+	},
+
+	{
+		page: "contact",
+		description:
+			"If you're interested in collaborating on a project, feel free to reach out to me. I'm always open to new ideas and opportunities.",
+		keywords: ["Aditya", "Aditya N.", "Aditya Negi"],
+	},
+];
+
+export default SEO;
